3b666116989d3c96afc9b1f3ab5d0f88ab89ca5f
talmeym <mtalmey@gmail.com>
2017-09-14 13:29:42 +0100

    Refactored loaders and yml loading

--- src/main/java/com/emarte/regurgitator/core/JsonLoader.java
+++ src/main/java/com/emarte/regurgitator/core/JsonLoader.java
@@ -4,6 +4,6 @@ import net.sf.json.JSONObject;
 
 import java.util.Set;
 
-public interface JsonLoader<TYPE> extends Loader {
-	public TYPE load(JSONObject jsonObject, Set<Object> allIds) throws RegurgitatorException;
+public interface JsonLoader<TYPE> extends Loader<JSONObject, TYPE> {
+	TYPE load(JSONObject jsonObject, Set<Object> allIds) throws RegurgitatorException;
 }
