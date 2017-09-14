ba1952f542f2e533dacc37b30e99d7c59abfc474
talmeym <mtalmey@gmail.com>
2017-09-14 18:07:27 +0100

    Generified LoaderUtils

--- src/main/java/com/emarte/regurgitator/core/JsonLoaderUtil.java
+++ src/main/java/com/emarte/regurgitator/core/JsonLoaderUtil.java
@@ -6,17 +6,21 @@ import static com.emarte.regurgitator.core.JsonConfigUtil.loadJsonType;
 import static com.emarte.regurgitator.core.JsonPackageLookup.getPackageForType;
 import static com.emarte.regurgitator.core.StringUtil.dashesToCamelCase;
 
-public class JsonLoaderUtil<TYPE extends Loader> extends LoaderUtil<TYPE> {
+public class JsonLoaderUtil<TYPE extends Loader> extends LoaderUtil<JSONObject, TYPE> {
+
+	@Override
 	public TYPE deriveLoader(JSONObject jsonObject) throws RegurgitatorException {
 		return buildFromClass(deriveClass(jsonObject));
 	}
 
-	static String deriveClass(JSONObject jsonObject) throws RegurgitatorException {
+	@Override
+	String deriveClass(JSONObject jsonObject) throws RegurgitatorException {
 		String type = loadJsonType(jsonObject);
 		return deriveClass(getPackageForType(type), dashesToCamelCase(type));
 	}
 
-	static String deriveClass(String packageName, String className) throws RegurgitatorException {
+	@Override
+	String deriveClass(String packageName, String className) throws RegurgitatorException {
 		if (packageName == null) {
 			throw new RegurgitatorException("Package not found for class: " + className);
 		}
